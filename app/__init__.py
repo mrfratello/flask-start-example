e64fad3907a2b3becb11a75dbaf8a35d713f7657
Syomochkin <info@syomochkin.xyz>
2017-02-23 00:16:58 +0300

    add flask-migration

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 apple = Flask(__name__)
 apple.config.from_object('config')
 db = SQLAlchemy(apple)
+migrate = Migrate(apple, db)
+
 import app.views
 import app.models
--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 CSRF_ENABLED = True
