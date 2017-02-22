2be1cb8b6ee003c7371d80bdc6dd1388909aa707
Syomochkin <info@syomochkin.xyz>
2017-02-23 00:24:17 +0300

    first migration script

--- config.py
+++ config.py
@@ -2,8 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'tmp/app.sqlite')
 
 CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+SECRET_KEY = 't38i8e+)*1aws7s7vgoq^khw=18u_j-k#6mhaj$b*gbq#(0#mv'
