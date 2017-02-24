5cc61d518942f56cb7ec6d9ac6f68fd636a52d45
Syomochkin <info@syomochkin.xyz>
2017-02-24 21:29:54 +0300

    template for add users

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ import hashlib
 from app import db
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     nickname = db.Column(db.String(64), index = True, unique = True)
     password = db.Column(db.String(255), index=True)
     email = db.Column(db.String(120), index = True, unique = True)
@@ -13,7 +13,10 @@ class User(db.Model):
         self.password = User.get_hash_password(password)
 
     def __repr__(self):
-        return '<User {nick}>'.format(nick=self.nickname)
+        return '<User nickname={nick} email={email}>'.format(
+            nick=self.nickname,
+            email=self.email
+        )
 
     @staticmethod
     def get_hash_password(password):
--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 import os
-from flask import abort, g
-from app import apple
+from flask import abort, render_template, redirect, url_for
+from app import apple, db
+from models import User
 
 @apple.route('/')
 def index():
@@ -15,9 +16,21 @@ def message(mess_id):
     else:
         abort(404)
 
+@apple.route('/users')
+def users():
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+@apple.route('/users/add_random')
+def add_user():
+    user = User('test', 'test_pass')
+    db.session.add(user)
+    db.session.commit()
+    return redirect(url_for('users'))
+
+
 
 @apple.errorhandler(404)
 def page_not_found(error):
     return "Error 404", 404
-
 
